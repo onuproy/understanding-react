0091a35aaf05157e4a4277f9b2dbdff9316f71bf
onuproy <anupme01@gmail.com>
2021-09-26 16:19:11 +0600

    title add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <h2>This is title</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
